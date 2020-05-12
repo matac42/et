69505074024dc2293a7e869922a663b66067ad16
yuto matayoshi <matac@matac42.local>
2020-05-12 18:09:03 +0900

    add main.go & index.html

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+    "gopkg.in/olahol/melody.v1"
+)
+
+func main() {
+    log.Println("Websocket App start.")
+
+    router := gin.Default()
+    m := melody.New()
+
+    rg := router.Group("/sampleapp")
+    rg.GET("/", func(ctx *gin.Context) {
+        http.ServeFile(ctx.Writer, ctx.Request, "index.html")
+    })
+
+    rg.GET("/ws", func(ctx *gin.Context) {
+        m.HandleRequest(ctx.Writer, ctx.Request)
+    })
+
+    m.HandleMessage(func(s *melody.Session, msg []byte) {
+        m.Broadcast(msg)
+    })
+
+    m.HandleConnect(func(s *melody.Session) {
+        log.Printf("websocket connection open. [session: %#v]\n", s)
+    })
+
+    m.HandleDisconnect(func(s *melody.Session) {
+        log.Printf("websocket connection close. [session: %#v]\n", s)
+    })
+
+    // Listen and server on 0.0.0.0:8989
+    router.Run(":8989")
+
+    fmt.Println("Websocket App End.")
+}
